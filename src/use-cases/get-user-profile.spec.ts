8d59b92c47d5bd1485f9eab54cf481774d2e5553
chain <klaus.mika1052@gmail.com>
2023-09-08 14:35:19 -0300

    Implementando casos de uso - Caso de uso de perfil

---
+++ src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,38 @@
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { GetUserProfileUseCase } from './get-user-profile'
+import { UsersRepository } from '@/repositories/users-repository'
+import { hash } from 'bcryptjs'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+describe('Get User Profile Use Case', () => {
+  let userRepository: UsersRepository
+  let sut: GetUserProfileUseCase
+
+  beforeEach(() => {
+    userRepository = new InMemoryUsersRepository()
+    sut = new GetUserProfileUseCase(userRepository)
+  })
+
+  it('should be able to get user profile', async () => {
+    const createdUser = await userRepository.create({
+      name: 'John Doe',
+      email: 'jdoe@example.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    const { user } = await sut.execute({
+      userId: createdUser.id,
+    })
+
+    expect(user.name).toEqual('John Doe')
+  })
+
+  it('should not be able to get ser profile with wrong id', async () => {
+    expect(() =>
+      sut.execute({
+        userId: 'non-existing-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
