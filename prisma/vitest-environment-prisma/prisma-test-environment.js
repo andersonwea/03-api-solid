24948d88ef3e0c1e9c753087b1beae03a845a288
chain <klaus.mika1052@gmail.com>
2023-09-12 20:15:57 -0300

    Controllers & Testes E2E - Criando test environment

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.js
@@ -0,0 +1,13 @@
+export default {
+  transformMode: 'ssr',
+  name: 'prisma',
+  async setup() {
+    console.log('Setup')
+
+    return {
+      async teardown() {
+        console.log('Teardown')
+      },
+    }
+  },
+}
