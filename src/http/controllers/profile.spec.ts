70766e58c7d69a8eadd2f65971c1b5fe8386f251
chain <klaus.mika1052@gmail.com>
2023-09-12 21:22:24 -0300

    Controllers & Testes E2E - Teste E2E do perfil

---
+++ src/http/controllers/profile.spec.ts
@@ -0,0 +1,39 @@
+import { afterAll, beforeAll, expect, it, test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+
+test('Profile (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'jdoe@example.com',
+      password: '123456',
+    })
+
+    const authResponse = await request(app.server).post('/sessions').send({
+      email: 'jdoe@example.com',
+      password: '123456',
+    })
+
+    const { token } = authResponse.body
+
+    const response = await request(app.server)
+      .get('/me')
+      .set('Authorization', `Bearer ${token}`)
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        email: 'jdoe@example.com',
+      }),
+    )
+  })
+})
