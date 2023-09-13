a6a51f39b5db2d21c2bcc9aa896a9a1e558c44c1
chain <klaus.mika1052@gmail.com>
2023-09-12 21:04:21 -0300

    Controllers & Testes E2E - Teste E2E do registro

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,23 @@
-import { test } from 'vitest'
+import { afterAll, beforeAll, expect, it, test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
 
-test('ok', () => {})
+test('ok', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'jdoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
