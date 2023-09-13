ca1e782ae7fb000bd02b13f99053c4fa049b0c34
chain <klaus.mika1052@gmail.com>
2023-09-13 20:40:26 -0300

    Refresh Token & RBAC - Estratégia de refresh de token

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,24 @@ import { ZodError } from 'zod'
 import { env } from './env'
 import fastifyJwt from '@fastify/jwt'
 import { usersRoutes } from './http/controllers/users/routes'
-import { gymsRoutes, gymsRoutes } from './http/controllers/gyms/routes'
+import { gymsRoutes } from './http/controllers/gyms/routes'
 import { checkInsRoutes } from './http/controllers/checkIns/routes'
+import fastifyCookie from '@fastify/cookie'
 
 export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
 })
+
+app.register(fastifyCookie)
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
